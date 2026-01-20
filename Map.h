cbd149b80daf3a5b66b51a8197ef6e2dcd1dd4dc
anythiinq <108547358+anythiinq@users.noreply.github.com>
2026-01-20 12:52:03 -0800

    Refactor Map class to use type aliases for key/value

--- Map.h
+++ Map.h
@@ -1,26 +1,34 @@
+#ifndef MAP_H
+#define MAP_H
+
+#include <string>
+
+using KeyType = std::string;
+using ValueType = double;
+
 class Map
 {
   public:
     Map();         // Create an empty map (i.e., one whose size() is 0).
 
-    bool empty();  // Return true if the map is empty, otherwise false.
+    bool empty() const;  // Return true if the map is empty, otherwise false.
 
-    int size();    // Return the number of key/value pairs in the map.
+    int size() const;    // Return the number of key/value pairs in the map.
 
-    bool insert(const std::string& key, const double& value);
+    bool insert(const KeyType& key, const ValueType& value);
       // If key is not equal to any key currently in the map and if the
       // key/value pair can be added to the map, then do so and return true.
       // Otherwise, make no change to the map and return false (indicating
       // that either the key is already in the map, or the map has a fixed
       // capacity and is full).
 
-    bool update(const std::string& key, const double& value);
+    bool update(const KeyType& key, const ValueType& value);
       // If key is equal to a key currently in the map, then make that key no
       // longer map to the value that it currently maps to, but instead map to
       // the value of the second parameter; in this case, return true.
       // Otherwise, make no change to the map and return false.
 
-    bool insertOrUpdate(const std::string& key, const double& value);
+    bool insertOrUpdate(const KeyType& key, const ValueType& value);
       // If key is equal to a key currently in the map, then make that key no
       // longer map to the value that it currently maps to, but instead map to
       // the value of the second parameter; in this case, return true.
@@ -30,22 +38,22 @@ class Map
       // that the key is not already in the map and the map has a fixed
       // capacity and is full).
 
-    bool erase(const std::string& key);
+    bool erase(const KeyType& key);
       // If key is equal to a key currently in the map, remove the key/value
       // pair with that key from the map and return true.  Otherwise, make
       // no change to the map and return false.
      
-    bool contains(const std::string& key);
+    bool contains(const KeyType& key) const;
       // Return true if key is equal to a key currently in the map, otherwise
       // false.
      
-    bool get(const std::string& key, double& value);
+    bool get(const KeyType& key, ValueType& value) const;
       // If key is equal to a key currently in the map, set value to the
       // value in the map that the key maps to, and return true.  Otherwise,
       // make no change to the value parameter of this function and return
       // false.
-     
-    bool get(int i, std::string& key, double& value);
+    
+    bool get(int i, KeyType& key, ValueType& value) const;
       // If 0 <= i < size(), copy into the key and value parameters the
       // key and value of the key/value pair in the map whose key is strictly
       // greater than exactly i keys in the map and return true.  Otherwise,
@@ -53,4 +61,16 @@ class Map
 
     void swap(Map& other);
       // Exchange the contents of this map with the other one.
+    
+  private:
+    static const int DEFAULT_MAX_ITEMS = 180;
+    struct Entry {
+        KeyType m_key;
+        ValueType m_value;
+    };
+    
+    Entry m_map[DEFAULT_MAX_ITEMS];
+    int m_size;
 };
+
+#endif
